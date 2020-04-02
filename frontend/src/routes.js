97f2e3f52d1c5042fbdf334754ae45a710b93354
Vinícius Morais <vcm1105@gmail.com>
2020-04-02 15:55:53 -0300

    rota da pagina pendings

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Pendings from './pages/Pendings';
 
 
 export default function Routes(){
@@ -11,6 +12,7 @@ export default function Routes(){
       <Switch>
         <Route path="/" exact component = {Login}/>  
         <Route path="/register" component = {Register}/>
+        <Route path="/pendings" component = {Pendings}/>
       </Switch>
     </BrowserRouter>
   );
