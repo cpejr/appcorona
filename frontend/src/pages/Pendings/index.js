d7758611dbb0455318f7999108e0631a9e7972b2
Vinícius Morais <vcm1105@gmail.com>
2020-04-02 20:23:39 -0300

    pagina com uma ong e pagina de todas pendentes

--- frontend/src/pages/Pendings/index.js
+++ frontend/src/pages/Pendings/index.js
@@ -1,169 +1,93 @@
 import React from 'react';
 import './styles.css';
-import ong from "./ong.png"
+
 
 export default function Pendings() {
     return (
         <div>
-            <h1 class="pendingsTitle">ONGs pendentes</h1>
-            <div class="gridCard">
-                <div class="ONGgrid">
-                    <div class="ONGcard">
-                        <div class="container">
-                            <img src={ong} alt="Foto da ONG" />
-                            <div class="forms-row">
-                                <div class="name">Nome da ONG/Projeto: </div>
-                                <div class="style-5">Nome que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Descrição da ONG:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row grid">
-                                <div class="info m-rg-45">Cidade:
-                                <div class="style-5">Cidade</div>
-                                </div>
-                                <div class="info">Estado:
-                                <div class="style-5">Estado</div>
-                                </div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Bairro:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Rua:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info m-rg-45">Número:
-                                <div class="style-5">Número</div>
-                                </div>
-                                    <div class="info">Complemento:
-                                    <div class="style-5">Complemento</div>
-                                </div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">CNPJ:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">PicPay:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Facebook:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Instagram:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Email:</div>
-                                <div class="style-5">Email</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info m-rg-45">DDD:
-                                    <div class="style-5">DDD</div>
-                                </div>
-                                <div class="info">Telefone:
-                                    <div class="style-5">Telefone</div>
-                                </div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info m-rg-45">Banco:
-                                        <div class="style-5">Banco</div>
-                                </div>
-                                <div class="info">Agência:
-                                    <div class="style-5">Agência</div></div>
-
-                            </div>
+            <div className="ONGcard">
+                <h1 className="pendingsTitle">ONG PENDENTE</h1>
+                <div className="container">
+                    <img src="ong.png" alt="Foto da ONG" />
+                    <div className="forms-row">
+                        <div className="name">Nome da ONG/Projeto: </div>
+                        <div className="style-5">Nome que vem do banco</div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info">Descrição da ONG:</div>
+                        <div className="style-5">Descrição que vem do banco</div>
+                    </div>
+                    <div className="forms-row grid">
+                        <div className="info m-rg-45">Cidade:
+                                <div className="style-5">Cidade</div>
                         </div>
-
+                        <div className="info">Estado:
+                                <div className="style-5">Estado</div>
+                        </div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info">Bairro:</div>
+                        <div className="style-5">Descrição que vem do banco</div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info">Rua/Avenida:</div>
+                        <div className="style-5">Descrição que vem do banco</div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info m-rg-45">Número:
+                                <div className="style-5">Número</div>
+                        </div>
+                        <div className="info m-rg-45">Complemento:
+                                    <div className="style-5">Complemento</div>
+                        </div>
+                        <div className="info m-rg-45">CEP:
+                                    <div className="style-5">00000-000</div>
+                        </div>
+                        <div className="info">CNPJ:
+                            <div className="style-5">Descrição que vem do banco</div>
+                        </div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info m-rg-45">PicPay:
+                        <div className="style-5">Descrição que vem do banco</div>
+                        </div>
+                        <div className="info">Facebook:
+                        <div className="style-5">Descrição que vem do banco</div>
+                        </div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info">Instagram:</div>
+                        <div className="style-5">Descrição que vem do banco</div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info">Email:</div>
+                        <div className="style-5">Email</div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info m-rg-45">DDD:
+                                    <div className="style-5">DDD</div>
+                        </div>
+                        <div className="info">Telefone:
+                                    <div className="style-5">Telefone</div>
+                        </div>
+                    </div>
+                    <div className="forms-row">
+                        <div className="info m-rg-45">Banco:
+                                        <div className="style-5">Banco</div>
+                        </div>
+                        <div className="info">Agência:
+                                    <div className="style-5">Agência</div></div>
 
                     </div>
-                </div>
+                    <div id="bttn">
+                    <button className="btn1 btn--green btn--radius m-rg-45" type="submit">APROVAR</button>
+                    <button className="btn1 btn--red btn--radius" type="submit">REJEITAR</button>
+                   </div>
 
-                <div class="ONGgrid">
-                    <div class="ONGcard">
-                        <div class="container">
-                            <img src={ong} alt="Foto da ONG" />
-                            <div class="forms-row">
-                                <div class="name">Nome da ONG/Projeto: </div>
-                                <div class="style-5">Nome que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Descrição da ONG:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row grid">
-                                <div class="info m-rg-45">Cidade:
-                                <div class="style-5">Cidade</div>
-                                </div>
-                                <div class="info">Estado:
-                                <div class="style-5">Estado</div>
-                                </div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Bairro:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Rua:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info m-rg-45">Número:
-                                <div class="style-5">Número</div>
-                                </div>
-                                    <div class="info">Complemento:
-                                    <div class="style-5">Complemento</div>
-                                </div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">CNPJ:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">PicPay:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Facebook:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Instagram:</div>
-                                <div class="style-5">Descrição que vem do banco</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info">Email:</div>
-                                <div class="style-5">Email</div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info m-rg-45">DDD:
-                                    <div class="style-5">DDD</div>
-                                </div>
-                                <div class="info">Telefone:
-                                    <div class="style-5">Telefone</div>
-                                </div>
-                            </div>
-                            <div class="forms-row">
-                                <div class="info m-rg-45">Banco:
-                                        <div class="style-5">Banco</div>
-                                </div>
-                                <div class="info">Agência:
-                                    <div class="style-5">Agência</div></div>
-
-                            </div>
-                        </div>
-
-
-                    </div>
                 </div>
 
 
+               
             </div>
         </div>
 
