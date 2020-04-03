e0792c2d517b6b693b2966a8da55e46bca67828b
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-04-03 20:02:13 -0300

    Logica de aprovar ong funcionando

--- frontend/src/pages/Pendings/index.js
+++ frontend/src/pages/Pendings/index.js
@@ -4,144 +4,153 @@ import api from '../../services/api';
 
 
 export default function Pendings() {
-    let ongs = []
-    [currentOng, setOng] = useState(0)
+  const [ongs, setOngs] = useState([]);
+  const [currentOng, setCurrentOng] = useState(0);
 
-    useEffect(async ()=>{
-        try {ongs = await api.get('admin')}
-        catch(err) {
-            //handle error
-        }
-
-    },[])
-
-    function handleNext() {
-        if (currentOng<ongs.length-1){
-            setOng(currentOng++);
-            return
-        }
-        else 
-            return
+  async function getOngs() {
+    try {
+      let ongsResponse = await api.get('admin', {
+        headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODU5NTQyNDEsImV4cCI6MTU4NjA0MDY0MX0.H-h_B3NRxlTGZI-ht4eN9VSWDqkf1XZVgxbgUQOcDwM` }
+      });
+      console.log(ongsResponse.data);
+      setOngs(ongsResponse.data);
     }
-
-    function handlePrev(){
-        if (currentOng!=0){
-            setOng(currentOng--);
-            return
-        }
-        else 
-            return
+    catch (err) {
+      console.warn(err);
     }
+  }
 
-    function handleApprove(e){
-        e.preventDefault()
-        let rota = 'admin/$(ongs[currentOng]._id)';
-        try {ongs = await api.post('admin/$(ongs[currentOng]._id)', "approved: true")}
-        catch(err){
-            alert("Erro");
-        }
+  useEffect(() => {
+    getOngs();
+  }, [])
+
+  function handleNext() {
+    if (currentOng < ongs.length - 1) {
+      setCurrentOng(currentOng + 1);
+      return
     }
+    else
+      return
+  }
 
-    function handleReject(e){
-        e.preventDefault()
-        let rota = 'admin/$(ongs[currentOng]._id)';
-        try {ongs = await api.delete('admin/$(ongs[currentOng]._id)')}
-        catch(err){
-            alert("Erro");
-        }
+  function handlePrev() {
+    if (currentOng < 0) {
+      setCurrentOng(currentOng - 1);
+      return
     }
+    else
+      return
+  }
 
+  async function handleApprove(e) {
+    e.preventDefault()
+    try {
+      await api.put(`admin/${ongs[currentOng]._id}`,
+        { approved: true },
+        {
+          headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODU5NTQyNDEsImV4cCI6MTU4NjA0MDY0MX0.H-h_B3NRxlTGZI-ht4eN9VSWDqkf1XZVgxbgUQOcDwM` }
+        });
+    }
+    catch (err) {
+      alert("Erro");
+    }
+  }
+
+  async function handleReject(e) {
+    e.preventDefault()
+    try {
+      await api.delete(`admin/${ongs[currentOng]._id}`, {
+        headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODU5NTQyNDEsImV4cCI6MTU4NjA0MDY0MX0.H-h_B3NRxlTGZI-ht4eN9VSWDqkf1XZVgxbgUQOcDwM` }
+      });
+    } catch (err) {
+      alert("Erro");
+    }
+  }
+  if (ongs[currentOng])
     return (
-        <div>
-            <div className="ONGcard">
-                <h1 className="pendingsTitle">ONG PENDENTE</h1>
-                <div className="container">
-                    <img src="ong.png" alt="Foto da ONG" />
-                    <div className="forms-row">
-                        <div className="name">Nome da ONG/Projeto: </div>
-                        <div className="style-5">{ongs[currentOng].name}</div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info">Descrição da ONG:</div>
-                        <div className="style-5">{ongs[currentOng].description}</div>
-                    </div>
-                    <div className="forms-row grid">
-                        <div className="info m-rg-45">Cidade:
-                                <div className="style-5">{ongs[currentOng].city}</div>
-                        </div>
-                        <div className="info">Estado:
-                                <div className="style-5">{ongs[currentOng].state}</div>
-                        </div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info">Bairro:</div>
-                        <div className="style-5">{ongs[currentOng].neighborhood}</div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info">Rua/Avenida:</div>
-                        <div className="style-5">{ongs[currentOng].street}</div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info m-rg-45">Número:
-                                <div className="style-5">{ongs[currentOng].number}</div>
-                        </div>
-                        <div className="info m-rg-45">Complemento:
-                                    <div className="style-5">{ongs[currentOng].complement}</div>
-                        </div>
-                        <div className="info m-rg-45">CEP:
-                                    <div className="style-5">{ongs[currentOng].CEP}</div>
-                        </div>
-                        <div className="info">CNPJ:
-                            <div className="style-5">{ongs[currentOng].cnpj}</div>
-                        </div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info m-rg-45">PicPay:
-                        <div className="style-5">{ongs[currentOng].picpay}</div>
-                        </div>
-                        <div className="info">Facebook:
-                        <div className="style-5">{ongs[currentOng].facebook}</div>
-                        </div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info">Instagram:</div>
-                        <div className="style-5">{ongs[currentOng].instagram}</div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info">Email:</div>
-                        <div className="style-5">{ongs[currentOng].email}</div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info m-rg-45">DDD:
-                            <div className="style-5">{ongs[currentOng].ddd}</div>
-                        </div>
-                        <div className="info">Telefone:
-                            <div className="style-5">{ongs[currentOng].phoneNumber}</div>
-                        </div>
-                    </div>
-                    <div className="forms-row">
-                        <div className="info m-rg-45">Banco:
-                            <div className="style-5">{ongs[currentOng].bank}</div>
-                        </div>
-                        <div className="info">Agência:
-                            <div className="style-5">{ongs[currentOng].agency}</div>
-                        </div>
-
-                    </div>
-                    <div id="bttn">
-                    <button className="btn1 btn--green btn--radius m-rg-45" onclick={handleApprove}>APROVAR</button>
-                    <button className="btn1 btn--red btn--radius" onclick={handleReject}>REJEITAR</button>
-                   </div>
-
-                </div>
-
-
-               
+      <div>
+        <div className="ONGcard">
+          <h1 className="pendingsTitle">ONG PENDENTE</h1>
+          <div className="container">
+            <img src="ong.png" alt="Foto da ONG" />
+            <div className="forms-row">
+              <div className="name">Nome da ONG/Projeto: </div>
+              <div className="style-5">{ongs[currentOng].name}</div>
             </div>
+            <div className="forms-row">
+              <div className="info">Descrição da ONG:</div>
+              <div className="style-5">{ongs[currentOng].description}</div>
+            </div>
+            <div className="forms-row grid">
+              <div className="info m-rg-45">Cidade:
+                <div className="style-5">{ongs[currentOng].city}</div>
+              </div>
+              <div className="info">Estado:
+                <div className="style-5">{ongs[currentOng].state}</div>
+              </div>
+            </div>
+            <div className="forms-row">
+              <div className="info">Bairro:</div>
+              <div className="style-5">{ongs[currentOng].neighborhood}</div>
+            </div>
+            <div className="forms-row">
+              <div className="info">Rua/Avenida:</div>
+              <div className="style-5">{ongs[currentOng].street}</div>
+            </div>
+            <div className="forms-row">
+              <div className="info m-rg-45">Número:
+                <div className="style-5">{ongs[currentOng].number}</div>
+              </div>
+              <div className="info m-rg-45">Complemento:
+                <div className="style-5">{ongs[currentOng].complement}</div>
+              </div>
+              <div className="info m-rg-45">CEP:
+                <div className="style-5">{ongs[currentOng].CEP}</div>
+              </div>
+              <div className="info">CNPJ:
+                <div className="style-5">{ongs[currentOng].cnpj}</div>
+              </div>
+            </div>
+            <div className="forms-row">
+              <div className="info m-rg-45">PicPay:
+               <div className="style-5">{ongs[currentOng].picpay}</div>
+              </div>
+              <div className="info">Facebook:
+                <div className="style-5">{ongs[currentOng].facebook}</div>
+              </div>
+            </div>
+            <div className="forms-row">
+              <div className="info">Instagram:</div>
+              <div className="style-5">{ongs[currentOng].instagram}</div>
+            </div>
+            <div className="forms-row">
+              <div className="info">Email:</div>
+              <div className="style-5">{ongs[currentOng].email}</div>
+            </div>
+            <div className="forms-row">
+              <div className="info m-rg-45">DDD:
+                <div className="style-5">{ongs[currentOng].ddd}</div>
+              </div>
+              <div className="info">Telefone:
+                <div className="style-5">{ongs[currentOng].phoneNumber}</div>
+              </div>
+            </div>
+            <div className="forms-row">
+              <div className="info m-rg-45">Banco:
+                <div className="style-5">{ongs[currentOng].bank}</div>
+              </div>
+              <div className="info">Agência:
+                <div className="style-5">{ongs[currentOng].agency}</div>
+              </div>
+            </div>
+            <div id="bttn">
+              <button className="btn1 btn--green btn--radius m-rg-45" onClick={handleApprove}>APROVAR</button>
+              <button className="btn1 btn--red btn--radius" onClick={handleReject}>REJEITAR</button>
+            </div>
+          </div>
         </div>
-
-
-
-
+      </div>
     );
+  else
+    return (<></>)
 }
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL:'http://192.168.1.5:3333/',
+    baseURL:'http://localhost:3333/',
 })
 
 export default api; 
