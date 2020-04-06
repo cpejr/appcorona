25e9caab1ed7b2b137589f3a5800939f267191cc
Arthur Martins <arthurmbraga1@gmail.com>
2020-04-06 18:13:28 -0300

    Revert "botão redirecionando para pag"

--- frontend/src/pages/AllPendings/index.js
+++ frontend/src/pages/AllPendings/index.js
@@ -13,69 +13,55 @@ export default function AllPendings() {
                             <img src="ong.png" alt="Foto da ONG" />
                             <div className="forms-row">
                                 <div className="name">Nome da ONG/Projeto: </div>
-                                <div className="info style-5">Nome que vem do banco</div>
+                                <div className="style-5">Nome que vem do banco</div>
                             </div>
                             <div className="forms-row">
                                 <div className="info">Descrição da ONG:</div>
-                                <div className="info style-5">Descrição que vem do banco</div>
+                                <div className="style-5">Descrição que vem do banco</div>
                             </div>
                             <div className="forms-row grid">
-                                <div className="info m-rg-20">Cidade:
+                                <div className="info m-rg-45">Cidade:
                                 <div className="style-5">Cidade</div>
                                 </div>
-                                <div className="info m-lt-20">Estado:
+                                <div className="info">Estado:
                                 <div className="style-5">Estado</div>
                                 </div>
                             </div>
                             <div id="bttn">
-                                <button 
-                                onClick={event => window.location.href='/pendings'}
-                                className="btn2 btn--blue btn--radius" 
-                                type="submit">ANALISAR APROVAÇÃO</button>
+                                <button className="btn2 btn--blue btn--radius" type="submit">ANALISAR APROVAÇÃO</button>
                             </div>
                         </div>
                     </div>                    
                 </div>
 
                 <div className="ONGgrid">
-                    <div className="ONGcard1">
+                    <div className="ONGcard">
                         <div className="container">
                             <img src="ong.png" alt="Foto da ONG" />
                             <div className="forms-row">
                                 <div className="name">Nome da ONG/Projeto: </div>
-                                <div className="info style-5">Nome que vem do banco</div>
+                                <div className="style-5">Nome que vem do banco</div>
                             </div>
                             <div className="forms-row">
                                 <div className="info">Descrição da ONG:</div>
-                                <div className="info style-5">Descrição que vem do banco</div>
+                                <div className="style-5">Descrição que vem do banco</div>
                             </div>
                             <div className="forms-row grid">
-                                <div className="info m-rg-20">Cidade:
+                                <div className="info m-rg-45">Cidade:
                                 <div className="style-5">Cidade</div>
                                 </div>
-                                <div className="info m-lt-20">Estado:
+                                <div className="info">Estado:
                                 <div className="style-5">Estado</div>
                                 </div>
                             </div>
                             <div id="bttn">
-                                <button 
-                                    onClick={event => window.location.href='/pendings'}
-                                    className="btn2 btn--blue btn--radius" 
-                                    type="submit">ANALISAR APROVAÇÃO
-                                    </button>
+                                <button className="btn2 btn--blue btn--radius" type="submit">ANALISAR APROVAÇÃO</button>
                             </div>
                         </div>
                     </div>                    
                 </div>
 
             </div>
-                    <div id="bttn">
-                     <button 
-                        onClick={event => window.location.href='/list'}
-                        className="btn1 btn--radius btn--blue"
-                        type="submit">VOLTAR A PÁGINA INICIAL
-                     </button>
-                     </div>
         </div>
 
 
