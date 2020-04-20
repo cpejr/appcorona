637862b011db2eacae71132b91d8e62b74c1b4a1
João Prates <jvdprates@hotmail.com>
2020-04-19 21:17:40 -0300

    Componente das checkboxes 70% pronto

--- frontend/src/components/Categ/CategContainer.js
+++ frontend/src/components/Categ/CategContainer.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Checkbox, FormGroup, FormControlLabel } from '@material-ui/core';
 
-export default function CategContainer({categName}) {
-    let id = "Id" + categName;
-    return(
-        <div className="container">
-            <input className="check" type="checkbox" id={id}/>
-            <label for={id}>{categName}</label>
-        </div>
+export default function CategContainer({ categNames, onChange }) {
+
+    const [state, setState] = useState({});
+    console.log(categNames);
+
+    useEffect(() => {
+        let newState = {};
+        for (let i = 0; i < categNames.length; i++) {
+            newState[categNames[i]] = false;
+        }
+        setState(newState);
+    }, [categNames])
+
+    const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
+
+    onChange(state);
+
+    return (
+        <FormGroup row>
+            {Object.keys(state).map((name) => {
+                return (
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={state[name]}
+                                onChange={handleChange}
+                                name={name}
+                                color="primary"
+                            />
+                        }
+                        label={name}
+                    />
+                )
+            })
+            }
+        </FormGroup>
     )
 }
--- frontend/src/pages/Pendings/Pending/OngCard.js
+++ frontend/src/pages/Pendings/Pending/OngCard.js
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Container from '../../../components/OngContainer'
 import { Link, Redirect } from 'react-router-dom';
 import api from '../../../services/api';
-import { Checkbox, FormGroup, FormControlLabel } from '@material-ui/core';
+import CategContainer from '../../../components/Categ/CategContainer'
+
 
 export default function OngCard(props) {
 
   const [finalized, setFinalized] = useState(false);
-  const [state, setState] = React.useState({
-    checkedA: true,
-    checkedB: true,
-    checkedC: true,
-    checkedD: true,
-  });
+  const [categVec, setCategVec] = useState([]);
+  const checkedVector = useRef([]);
 
   let ong;
   let token;
@@ -22,126 +19,102 @@ export default function OngCard(props) {
     token = props.location.state.token;
   }
 
+  useEffect(() => {
+    api.get('categ').then((categNamesResponse) => {
+      console.log(categNamesResponse.data);
+      setCategVec(categNamesResponse.data);
+    });
+  }, []);
+
+  const handleCheck = async (state) => {
+    let auxVector = [];
+    Object.keys(state).forEach((value) => {
+      if (state[value]) {
+        auxVector.push(value);
+      }
+    })
+    checkedVector.current = auxVector;
+  }
+
   const handleApproved = async (ong) => {
     try {
+      console.log(ong._id);
       await api.put(`admin/${ong._id}`,
         { approved: true },
         {
           headers: { Authorization: `Bearer ${token}` }
         });
-      alert('Aprovado com sucesso!');
-      setFinalized(true);
-    }
+      await api.put('categ',
+        { ong: {_id: ong._id}, categ: checkedVector},
+        {
+          headers: { Authorization: `Bearer ${token}` }
+        }
+  );
+    alert('Aprovado com sucesso!');
+    setFinalized(true);
+  }
     catch (err) {
-      console.warn(err);
-      alert("Erro");
-    }
+    console.warn(err);
+    alert("Erro");
   }
+}
 
-  const handleRejected = async (ong) => {
-    try {
-      await api.delete(`admin/${ong._id}`, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
-      alert('Deletado com sucesso!');
-      setFinalized(true);
-    } catch (err) {
-      console.warn(err);
-      alert("Erro");
-    }
+const handleRejected = async (ong) => {
+  try {
+    await api.delete(`admin/${ong._id}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    alert('Deletado com sucesso!');
+    setFinalized(true);
+  } catch (err) {
+    console.warn(err);
+    alert("Erro");
   }
+}
 
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
 
-  return (
-    <div>
-      {ong && !finalized ? (
-        <div className="ONGcard">
 
-          <div id="bttn" style={{ "margin-bottom": "20px" }}>
-            <Link className="btn1 btn--radius btn--blue" to={{
-              pathname: '/Pendings',
-              state: {
-                token: token
-              }
-            }}>
-              VOLTAR
-            </Link>
-          </div>
-          <h1 className="pendingsTitle">ONG PENDENTE</h1>
-          <Container ong={ong} />
+return (
+  <div>
+    {ong && !finalized ? (
+      <div className="ONGcard">
 
-          <FormGroup row>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={state.checkedA}
-                  onChange={handleChange}
-                  name="checkedA"
-                  color="primary"
-                />
-              }
-              label="Teste1"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={state.checkedB}
-                  onChange={handleChange}
-                  name="checkedB"
-                  color="primary"
-                />
-              }
-              label="Teste2"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={state.checkedC}
-                  onChange={handleChange}
-                  name="checkedC"
-                />
-              }
-              label="Teste3"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={state.checkedD}
-                  onChange={handleChange}
-                  name="checkedD"
-                  color="secondary"
-                />
-              }
-              label="Teste4"
-            />
-          </FormGroup>
-
-          <div id="bttn">
-            <button
-              onClick={() => handleApproved(ong)}
-              className="btn1 btn--green btn--radius m-rg-20"
-              type="submit">
-              APROVAR
-          </button>
-            <button
-              onClick={() => handleRejected(ong)}
-              className="btn1 btn--red btn--radius m-lt-20"
-              type="submit">
-              REJEITAR
-            </button>
-          </div>
-        </div>
-      ) : (
-          <Redirect to={{
+        <div id="bttn" style={{ "margin-bottom": "20px" }}>
+          <Link className="btn1 btn--radius btn--blue" to={{
             pathname: '/Pendings',
             state: {
               token: token
             }
-          }} />
-        )}
-    </div>
-  )
+          }}>
+            VOLTAR
+            </Link>
+        </div>
+        <h1 className="pendingsTitle">ONG PENDENTE</h1>
+        <Container ong={ong} />
+        <CategContainer categNames={categVec} onChange={handleCheck} />
+        <div id="bttn">
+          <button
+            onClick={() => handleApproved(ong)}
+            className="btn1 btn--green btn--radius m-rg-20"
+            type="submit">
+            APROVAR
+          </button>
+          <button
+            onClick={() => handleRejected(ong)}
+            className="btn1 btn--red btn--radius m-lt-20"
+            type="submit">
+            REJEITAR
+            </button>
+        </div>
+      </div>
+    ) : (
+        <Redirect to={{
+          pathname: '/Pendings',
+          state: {
+            token: token
+          }
+        }} />
+      )}
+  </div>
+)
 }
