76c519c90530ad2344e18914d571d7d3258c70e1
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-04-19 21:29:29 -0300

    Categ funcionando

--- frontend/src/pages/Pendings/Pending/OngCard.js
+++ frontend/src/pages/Pendings/Pending/OngCard.js
@@ -34,6 +34,7 @@ export default function OngCard(props) {
       }
     })
     checkedVector.current = auxVector;
+
   }
 
   const handleApproved = async (ong) => {
@@ -44,77 +45,78 @@ export default function OngCard(props) {
         {
           headers: { Authorization: `Bearer ${token}` }
         });
+
       await api.put('categ',
-        { ong: {_id: ong._id}, categ: checkedVector},
+        { ong: { _id: ong._id }, categ: checkedVector.current },
         {
           headers: { Authorization: `Bearer ${token}` }
         }
-  );
-    alert('Aprovado com sucesso!');
-    setFinalized(true);
-  }
+      );
+      alert('Aprovado com sucesso!');
+      setFinalized(true);
+    }
     catch (err) {
-    console.warn(err);
-    alert("Erro");
+      console.warn(err);
+      alert("Erro");
+    }
   }
-}
 
-const handleRejected = async (ong) => {
-  try {
-    await api.delete(`admin/${ong._id}`, {
-      headers: { Authorization: `Bearer ${token}` }
-    });
-    alert('Deletado com sucesso!');
-    setFinalized(true);
-  } catch (err) {
-    console.warn(err);
-    alert("Erro");
+  const handleRejected = async (ong) => {
+    try {
+      await api.delete(`admin/${ong._id}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      alert('Deletado com sucesso!');
+      setFinalized(true);
+    } catch (err) {
+      console.warn(err);
+      alert("Erro");
+    }
   }
-}
 
 
 
-return (
-  <div>
-    {ong && !finalized ? (
-      <div className="ONGcard">
+  return (
+    <div>
+      {ong && !finalized ? (
+        <div className="ONGcard">
 
-        <div id="bttn" style={{ "margin-bottom": "20px" }}>
-          <Link className="btn1 btn--radius btn--blue" to={{
+          <div id="bttn" style={{ "margin-bottom": "20px" }}>
+            <Link className="btn1 btn--radius btn--blue" to={{
+              pathname: '/Pendings',
+              state: {
+                token: token
+              }
+            }}>
+              VOLTAR
+            </Link>
+          </div>
+          <h1 className="pendingsTitle">ONG PENDENTE</h1>
+          <Container ong={ong} />
+          <CategContainer categNames={categVec} onChange={(state) => handleCheck(state)} />
+          <div id="bttn">
+            <button
+              onClick={() => handleApproved(ong)}
+              className="btn1 btn--green btn--radius m-rg-20"
+              type="submit">
+              APROVAR
+          </button>
+            <button
+              onClick={() => handleRejected(ong)}
+              className="btn1 btn--red btn--radius m-lt-20"
+              type="submit">
+              REJEITAR
+            </button>
+          </div>
+        </div>
+      ) : (
+          <Redirect to={{
             pathname: '/Pendings',
             state: {
               token: token
             }
-          }}>
-            VOLTAR
-            </Link>
-        </div>
-        <h1 className="pendingsTitle">ONG PENDENTE</h1>
-        <Container ong={ong} />
-        <CategContainer categNames={categVec} onChange={handleCheck} />
-        <div id="bttn">
-          <button
-            onClick={() => handleApproved(ong)}
-            className="btn1 btn--green btn--radius m-rg-20"
-            type="submit">
-            APROVAR
-          </button>
-          <button
-            onClick={() => handleRejected(ong)}
-            className="btn1 btn--red btn--radius m-lt-20"
-            type="submit">
-            REJEITAR
-            </button>
-        </div>
-      </div>
-    ) : (
-        <Redirect to={{
-          pathname: '/Pendings',
-          state: {
-            token: token
-          }
-        }} />
-      )}
-  </div>
-)
+          }} />
+        )}
+    </div>
+  )
 }
