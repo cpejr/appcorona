00f9c4fcfe087971cdff06bc8d17d9c2d50d0e34
Arthur <arthurdeolima@gmail.com>
2020-04-22 20:08:24 -0300

    Pagina quem somos

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import ReactPageScroller from "react-page-scroller";
-import './styles.sass';
+import './styles.css';
 
 import Page1 from './Page1';
-import Page2 from './Page2';
+import QuemSomos from './QuemSomos';
 import Page3 from './Page3';
 
 export default function Home(props) {
@@ -20,7 +20,7 @@ export default function Home(props) {
         customPageNumber={currentPage}
       >
         <Page1 />
-        <Page2 />
+        <QuemSomos />
         <Page3 />
         <Page1 />
       </ReactPageScroller>
